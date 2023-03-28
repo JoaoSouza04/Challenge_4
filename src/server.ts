339889094c3942e9a05884a86c40eb1e99fbbcd7
João Pedro <joaosvieira04@gmail.com>
2023-03-28 16:52:21 -0300

    P1_configurações - Adaptando o arquivo para conectar com o Banco de Dados

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,17 @@
 import 'reflect-metadata';
 const express = require("express");
-const app = express();
+import { myDataSource } from "../app-data-source";
 
+myDataSource
+  .initialize()
+  .then(() => {
+    console.log("Data Source has been initialized!")
+  })
+  .catch((err) => {
+    console.error("Error during Data Source initialization:", err)
+  })
+
+const app = express();
 app.use(express.json());
 
 app.listen(3000, () => {

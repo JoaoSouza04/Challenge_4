df7017ec97dce6635a2c882cae7ed094c45f32f9
João Pedro <joaosvieira04@gmail.com>
2023-03-29 18:42:36 -0300

    P1_Controllers - Arrumandos o controller de carros

--- src/controllers/carController.ts
+++ src/controllers/carController.ts
@@ -5,39 +5,41 @@ import { Car } from "../db/entities/car.entity";
 export const getAllCars = async (req: Request, res: Response) => {
   const results = await myDataSource.getRepository(Car).findBy(
     { license_plate: req.params.id });
-  return res.json(results)
+  return res.json(results);
 }
 
 export const createCar = async (req: Request, res: Response) => {
-  const car = await myDataSource.getRepository(Car).create({
-    license_plate: req.params.id,
-    model: req.body.model,
-    year: req.body.year,
-    manufacturer: req.body.manufacturer,
-    color: req.body.color
-  });
+  const car = await myDataSource.getRepository(Car).create(req.body);
+  await myDataSource.getRepository(Car).save(car);
   return res.json(car);
 }
 
 export const updateCar = async (req: Request, res: Response) => {
   const car = await myDataSource.getRepository(Car).findOneBy({
-    license_plate: req.params.id,
+    carId: req.params.carId,
   });
-  myDataSource.getRepository(Car).merge(car, req.body)
+  console.log(car)
+  await myDataSource.getRepository(Car).merge(car, req.body)
   const results = await myDataSource.getRepository(Car).save(car);
-  return res.send(results);
+  return res.json(results);
 }
 
 export const deleteCar = async (req: Request, res: Response) => {
+  const car = await myDataSource.getRepository(Car).findBy({ carId: req.params.carId });
   const results = await myDataSource.getRepository(Car).delete(
-    { license_plate: req.params.id }
-  );
-  return res.json(results)
+    { carId: req.params.carId });
+
+  if (results.affected == 0) return res.status(404).send("Car not found, please type another id!");
+
+  return res.json({
+    message: "This is what has been deleted!",
+    Car: car
+  })
 }
 
 export const getOneCar = async (req: Request, res: Response) => {
   const results = await myDataSource.getRepository(Car).findBy(
-    { license_plate: req.params.id }
+    { carId: req.params.carId }
   )
   return res.json(results);
 }

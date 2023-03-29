bf1a70e567f508176a1c31b5650979ae9c8fdbe5
João Pedro <joaosvieira04@gmail.com>
2023-03-29 09:38:05 -0300

    P1_banco de dados - Alterando o campo de entities do data source

--- app-data-source.ts
+++ app-data-source.ts
@@ -1,13 +1,15 @@
 import { DataSource } from "typeorm"
+import { Client } from "./src/db/entities/client.entity";
+import { Car } from "./src/db/entities/car.entity";
 
 export const myDataSource = new DataSource({
   type: "postgres",
   host: "localhost",
-  port: 3000,
+  port: 5432,
   username: "node_user",
   password: "node_user",
   database: "challenge_4",
-  entities: ["src/entity/*.js"],
+  entities: [Client, Car],
   logging: true,
   synchronize: true,
 })

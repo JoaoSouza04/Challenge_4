738ea7cc44d58e376db94f8a2be47320730961ed
João Pedro <joaosvieira04@gmail.com>
2023-03-29 18:45:23 -0300

    P1_Bando de Dados - Adicionando a entidade dos mecanicos

---
+++ src/db/entities/mechanic.entity.ts
@@ -0,0 +1,45 @@
+import { Entity, Column, PrimaryGeneratedColumn, BeforeInsert, BeforeUpdate } from "typeorm";
+import bcrypt from "bcryptjs";
+
+@Entity()
+export class Mechanic {
+
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  cpf: string;
+
+  @Column()
+  birthday: string;
+
+  @Column()
+  phone: string;
+
+  @Column()
+  email: string;
+
+  @Column("varchar", { array: true })
+  specialities: Array<string>;
+
+  @Column()
+  hiringDate: Date
+
+  @Column()
+  serviceFee: number
+
+  @Column()
+  status: string
+
+  @Column()
+  password: string
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  hashPassword() {
+    this.password = bcrypt.hashSync(this.password, 8);
+  }
+}

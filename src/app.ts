46c7f927cd4ab0435028c80599284f0803df28d9
João Pedro <joaosvieira04@gmail.com>
2023-03-29 18:48:02 -0300

    P1_configurações - Corrigindo bugs no arquivo principal

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import 'reflect-metadata';
+const express = require("express");
+import { myDataSource } from "../app-data-source";
+import routes from "./routes"
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './.env' });
+
+myDataSource
+  .initialize()
+  .then(() => {
+    console.log("Data Source has been initialized!")
+  })
+  .catch((err) => {
+    console.error("Error during Data Source initialization:", err)
+  })
+
+const app = express();
+app.use(express.json());
+app.use(routes);
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000!");
+})

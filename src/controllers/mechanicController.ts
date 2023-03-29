e84b7a96df0f745354aa4287485f13f018c080e7
João Pedro <joaosvieira04@gmail.com>
2023-03-29 18:41:34 -0300

    P1_Controllers - adicionando os controller dos mecanicos

---
+++ src/controllers/mechanicController.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from "express";
+import { myDataSource } from "../../app-data-source";
+import { Mechanic } from "../db/entities/mechanic.entity";
+
+export const getAllMechanics = async (req: Request, res: Response) => {
+  const mechanics = await myDataSource.getRepository(Mechanic).find();
+  res.json(mechanics);
+}
+
+export const createMechanic = async (req: Request, res: Response) => {
+  const mechanic = await myDataSource.getRepository(Mechanic).create(req.body);
+  await myDataSource.getRepository(Mechanic).save(mechanic);
+  return res.json(mechanic);
+}
+
+export const getOneMechanic = async (req: Request, res: Response) => {
+  const results = await myDataSource.getRepository(Mechanic).findOneBy({
+    id: req.params.id,
+  })
+  return res.json(results);
+}
+
+export const updateMechanic = async (req: Request, res: Response) => {
+  const mechanic = await myDataSource.getRepository(Mechanic).findOneBy({
+    id: req.params.id,
+  });
+  await myDataSource.getRepository(Mechanic).merge(mechanic, req.body)
+  const results = await myDataSource.getRepository(Mechanic).save(mechanic);
+  return res.json(results);
+}

d2bef333ffd68bb0ff5335cfdfb4de570c9eb027
João Pedro <joaosvieira04@gmail.com>
2023-03-29 21:28:42 -0300

    P1_Rotas - Adicionando as rotas de services

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,12 @@
 import { Router, Request, Response } from "express";
 import { getAllClients, createClient, updateClient, getOneClient } from "./controllers/clientController";
 import { getAllCars, createCar, updateCar, deleteCar, getOneCar } from "./controllers/carController";
+import { getAllParts, createPart, updatePart, getOnePart } from "./controllers/partController";
 import { getAllMechanics, createMechanic, updateMechanic, getOneMechanic }
   from "./controllers/mechanicController";
-import { getAllParts, createPart, updatePart, getOnePart } from "./controllers/partController";
-import { loginClient } from "./controllers/authController";
+import { getAllServices, createService, updateService, getOneService }
+  from "./controllers/serviceController";
+// import { loginClient } from "./controllers/authController";
 const routes = Router();
 
 routes.get("/", (req: Request, res: Response) => {
@@ -27,11 +29,16 @@ routes.post("/api/v1/mechanics", createMechanic);
 routes.patch("/api/v1/mechanics/:partId", updateMechanic);
 routes.get("/api/v1/mechanics/:partId", getOneMechanic);
 
-routes.get("/api/v1/clients", getAllParts);
-routes.post("/api/v1/clients", createPart);
-routes.patch("/api/v1/clients/:id", updatePart);
-routes.get("/api/v1/clients/:id", getOnePart);
+routes.get("/api/v1/parts", getAllParts);
+routes.post("/api/v1/parts", createPart);
+routes.patch("/api/v1/parts/:id", updatePart);
+routes.get("/api/v1/parts/:id", getOnePart);
 
-routes.post("/api/v1/client/login", loginClient);
+routes.get("/api/v1/services", getAllServices);
+routes.post("/api/v1/services", createService);
+routes.patch("/api/v1/services/:id", updateService);
+routes.get("/api/v1/services/:id", getOneService);
+
+// routes.post("/api/v1/client/login", loginClient);
 
 export default routes;

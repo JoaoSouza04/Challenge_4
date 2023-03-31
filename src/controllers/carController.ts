aeae610eace8687b01450b4ae618a2c3cfca1dde
João Pedro <joaosvieira04@gmail.com>
2023-03-30 22:14:29 -0300

    P1_Controllers - Implementando a validação na rota de create

--- src/controllers/carController.ts
+++ src/controllers/carController.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import { myDataSource } from "../../app-data-source";
 import { Car } from "../db/entities/car.entity";
+import { Client } from "../db/entities/client.entity";
+import { validateCarData } from "../services/createCarValidation";
 
 export const getAllCars = async (req: Request, res: Response) => {
   const results = await myDataSource.getRepository(Car).findBy(
@@ -9,6 +11,18 @@ export const getAllCars = async (req: Request, res: Response) => {
 }
 
 export const createCar = async (req: Request, res: Response) => {
+  const client = await myDataSource.getRepository(Client).findOne(
+    { where: { id: req.body.clientId } });
+
+  if (!client) return res.status(400).send("Invalid or not found id!");
+
+  if (req.body.ownerId != req.params.id) {
+    return res.status(400).send("The id of the URL and the ownerId aren't the same!")
+  }
+
+  const valid = validateCarData(req.body);
+  if (valid.error) return res.status(400).send("The fields aren't valid!");
+
   const car = await myDataSource.getRepository(Car).create(req.body);
   await myDataSource.getRepository(Car).save(car);
   return res.json(car);
@@ -16,15 +30,34 @@ export const createCar = async (req: Request, res: Response) => {
 
 export const updateCar = async (req: Request, res: Response) => {
   const car = await myDataSource.getRepository(Car).findOneBy({
-    carId: req.params.carId,
+    carId: req.params.carId
   });
-  console.log(car)
+
+  const client = await myDataSource.getRepository(Client).findOne(
+    { where: { id: req.body.ownerId } });
+
+  if (!client) return res.status(400).send("Invalid or not found owner id!");
+
+  if (req.body.ownerId != req.params.id) {
+    return res.status(400).send("The id of the URL and the ownerId aren't the same!")
+  }
+
   await myDataSource.getRepository(Car).merge(car, req.body)
   const results = await myDataSource.getRepository(Car).save(car);
   return res.json(results);
 }
 
 export const deleteCar = async (req: Request, res: Response) => {
+
+  const client = await myDataSource.getRepository(Client).findOne(
+    { where: { id: req.body.ownerId } });
+
+  if (req.body.ownerId != req.params.id) {
+    return res.status(400).send("The id of the URL and the ownerId aren't the same!")
+  }
+
+  if (!client) return res.status(400).send("Invalid or not found owner id!");
+
   const car = await myDataSource.getRepository(Car).findBy({ carId: req.params.carId });
   const results = await myDataSource.getRepository(Car).delete(
     { carId: req.params.carId });

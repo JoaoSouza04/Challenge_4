aec2c90e3d8b1e497df35f63de5bc67b823e7704
João Pedro <joaosvieira04@gmail.com>
2023-03-30 22:12:23 -0300

    P1_Services - Implementando a validação de criação de Carros

---
+++ src/services/createCarValidation.ts
@@ -0,0 +1,22 @@
+import Joi from "joi";
+
+export const validateCarData = (data: {
+  ownerId: string;
+  license_plate: string;
+  model: string;
+  year: number;
+  manufacturer: string
+  color: string;
+}) => {
+
+  const createSchema = Joi.object({
+    ownerId: Joi.string().required(),
+    license_plate: Joi.string().required(),
+    model: Joi.string().required(),
+    year: Joi.number().required(),
+    manufacturer: Joi.string().required(),
+    color: Joi.string().max(30).required(),
+  })
+
+  return createSchema.validate(data);
+}

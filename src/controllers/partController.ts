ee7a0ceffe1ea90461e254b49f7d1929b625f727
João Pedro <joaosvieira04@gmail.com>
2023-03-30 23:13:44 -0300

    P1_Controllers - Implementando a validação na rota de update

--- src/controllers/partController.ts
+++ src/controllers/partController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { myDataSource } from "../../app-data-source";
 import { Part } from "../db/entities/part.entity";
 import { validatePartData } from "../services/createPartValidation";
+import { validatePartUpdate } from "../services/updatePartValidation";
 
 export const getAllParts = async (req: Request, res: Response) => {
   const parts = await myDataSource.getRepository(Part).find();
@@ -26,6 +27,10 @@ export const getOnePart = async (req: Request, res: Response) => {
 }
 
 export const updatePart = async (req: Request, res: Response) => {
+
+  const valid = validatePartUpdate(req.body);
+  if (valid.error) return res.status(400).send("The fields aren't valid!");
+
   const part = await myDataSource.getRepository(Part).findOneBy({
     partId: req.params.partId,
   });

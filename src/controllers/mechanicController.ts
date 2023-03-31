928c47e46c08f0e0ffa2f28b8aa9903daf53a3cf
João Pedro <joaosvieira04@gmail.com>
2023-03-30 21:36:16 -0300

    P1_Controllers - Implementando a validação de create mechanic nas rotas

--- src/controllers/mechanicController.ts
+++ src/controllers/mechanicController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { myDataSource } from "../../app-data-source";
 import { Mechanic } from "../db/entities/mechanic.entity";
+import { validateMechanicData } from "../services/createMechanicValidation";
 
 export const getAllMechanics = async (req: Request, res: Response) => {
   const mechanics = await myDataSource.getRepository(Mechanic).find();
@@ -13,6 +14,10 @@ export const getAllMechanics = async (req: Request, res: Response) => {
 }
 
 export const createMechanic = async (req: Request, res: Response) => {
+
+  const valid = validateMechanicData(req.body);
+  if (valid.error) return res.status(400).send("The fields aren't valid!");
+
   const mechanic = await myDataSource.getRepository(Mechanic).create(req.body);
   await myDataSource.getRepository(Mechanic).save(mechanic);
   return res.json(mechanic);

1e39b3429b512712a2ee5cf9bc0d0ca7e79fde13
João Pedro <joaosvieira04@gmail.com>
2023-03-30 22:40:03 -0300

    P1_Controllers - Implementando a validação na rota de update

--- src/services/updateCarValidation.ts
+++ src/services/updateCarValidation.ts
@@ -11,7 +11,7 @@ export const validateCarUpdate = (data: {
 
   const createSchema = Joi.object({
     ownerId: Joi.string().required(),
-    license_plate: Joi.string().required(),
+    license_plate: Joi.string().min(7).max(7).required(),
     model: Joi.string().required(),
     year: Joi.number().required(),
     manufacturer: Joi.string().required(),

3c7b694abb9f97c675ed14b7dd9ac3ab219cd0b5
João Pedro <joaosvieira04@gmail.com>
2023-04-05 10:55:41 -0300

    P2_Controllers - Tirando a validação de requests dos controllers

--- src/controllers/partController.ts
+++ src/controllers/partController.ts
@@ -1,8 +1,6 @@
 import { Request, Response } from "express";
 import { myDataSource } from "../../app-data-source";
 import { Part } from "../db/entities/part.entity";
-import { validatePartData } from "../services/createPartValidation";
-import { validatePartUpdate } from "../services/updatePartValidation";
 
 export const getAllParts = async (req: Request, res: Response) => {
   const parts = await myDataSource.getRepository(Part).find();
@@ -10,10 +8,6 @@ export const getAllParts = async (req: Request, res: Response) => {
 }
 
 export const createPart = async (req: Request, res: Response) => {
-
-  const valid = validatePartData(req.body);
-  if (valid.error) return res.status(400).send("The fields aren't valid!");
-
   const part = await myDataSource.getRepository(Part).create(req.body);
   await myDataSource.getRepository(Part).save(part);
   return res.json(part);
@@ -27,10 +21,6 @@ export const getOnePart = async (req: Request, res: Response) => {
 }
 
 export const updatePart = async (req: Request, res: Response) => {
-
-  const valid = validatePartUpdate(req.body);
-  if (valid.error) return res.status(400).send("The fields aren't valid!");
-
   const part = await myDataSource.getRepository(Part).findOneBy({
     partId: req.params.partId,
   });

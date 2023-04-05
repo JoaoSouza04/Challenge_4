c7c5310d20480b45dd78c577ff00c4f5ef1337fc
João Pedro <joaosvieira04@gmail.com>
2023-04-05 10:52:18 -0300

    P2_Rotas - Adicionando a proteção feita nas rotas desejadas

--- src/routes.ts
+++ src/routes.ts
@@ -8,43 +8,52 @@ import { getAllServices, createService, updateService, getOneService }
   from "./controllers/serviceController";
 
 import { loginClient, loginMechanic, refreshClientToken, refreshMechanicToken, updateClientPassword, updateMechanicPassword } from "./controllers/authController";
+
+import { authClientToken, authGeneralToken, authMechanicToken } from "./middlewares/auth";
+import { validCreateClient, validUpdateClient } from "./middlewares/validateClientFields";
+import { validCreateCar, validUpdateCar } from "./middlewares/validateCarFields";
+import { validCreateMechanic, validUpdateMechanic } from "./middlewares/validateMechanicFields";
+import { validCreatePart, validUpdatePart } from "./middlewares/validatePartsFields";
+import { validCreateService, validUpdateService } from "./middlewares/validateServiceFields";
+
 const routes = Router();
 
-routes.get("/", (req: Request, res: Response) => {
+routes.get("/api/v1/", (req: Request, res: Response) => {
   return res.json({ message: "Welcome to the Auto Repair Shop!" });
 })
 
 routes.get("/api/v1/clients", getAllClients);
-routes.post("/api/v1/clients", createClient);
-routes.patch("/api/v1/clients/:id", updateClient);
-routes.get("/api/v1/clients/:id", getOneClient);
+routes.post("/api/v1/clients", validCreateClient, createClient);
+routes.patch("/api/v1/clients/:id", authClientToken, validUpdateClient, updateClient);
+routes.get("/api/v1/clients/:id", authClientToken, getOneClient);
 
-routes.get("/api/v1/clients/:id/cars", getAllCars);
-routes.post("/api/v1/clients/:id/cars", createCar);
-routes.patch("/api/v1/clients/:id/cars/:carId", updateCar);
-routes.delete("/api/v1/clients/:id/cars/:carId", deleteCar);
-routes.get("/api/v1/clients/:id/cars/:carId", getOneCar);
+routes.get("/api/v1/clients/:id/cars", authGeneralToken, getAllCars);
+routes.post("/api/v1/clients/:id/cars", authClientToken, validCreateCar, createCar);
+routes.patch("/api/v1/clients/:id/cars/:carId", authClientToken, validUpdateCar, updateCar);
+routes.delete("/api/v1/clients/:id/cars/:carId", authClientToken, deleteCar);
+routes.get("/api/v1/clients/:id/cars/:carId", authGeneralToken, getOneCar);
 
 routes.get("/api/v1/mechanics", getAllMechanics);
-routes.post("/api/v1/mechanics", createMechanic);
-routes.patch("/api/v1/mechanics/:partId", updateMechanic);
-routes.get("/api/v1/mechanics/:partId", getOneMechanic);
+routes.post("/api/v1/mechanics", validCreateMechanic, createMechanic);
+routes.patch("/api/v1/mechanics/:id", authMechanicToken, validUpdateMechanic, updateMechanic);
+routes.get("/api/v1/mechanics/:id", authMechanicToken, getOneMechanic);
 
-routes.get("/api/v1/parts", getAllParts);
-routes.post("/api/v1/parts", createPart);
-routes.patch("/api/v1/parts/:id", updatePart);
-routes.get("/api/v1/parts/:id", getOnePart);
+routes.get("/api/v1/parts", authMechanicToken, getAllParts);
+routes.post("/api/v1/parts", authMechanicToken, validCreatePart, createPart);
+routes.patch("/api/v1/parts/:id", authMechanicToken, validUpdatePart, updatePart);
+routes.get("/api/v1/parts/:id", authMechanicToken, getOnePart);
 
-routes.get("/api/v1/services", getAllServices);
-routes.post("/api/v1/services", createService);
-routes.patch("/api/v1/services/:id", updateService);
-routes.get("/api/v1/services/:id", getOneService);
+routes.get("/api/v1/services", authGeneralToken, getAllServices);
+routes.post("/api/v1/services", authMechanicToken, validCreateService, createService);
+routes.patch("/api/v1/services/:id", authMechanicToken, validUpdateService, updateService);
+routes.get("/api/v1/services/:id", authGeneralToken, getOneService);
 
 routes.post("/api/v1/client/login", loginClient);
-routes.post("/api/v1/client/updatePassword", updateClientPassword);
+routes.post("/api/v1/client/updatePassword", authClientToken, updateClientPassword);
 routes.post("/api/v1/client/refreshToken", refreshClientToken);
+
 routes.post("/api/v1/mechanic/login", loginMechanic);
-routes.post("/api/v1/mechanic/updatePassword", updateMechanicPassword);
+routes.post("/api/v1/mechanic/updatePassword", authMechanicToken, updateMechanicPassword);
 routes.post("/api/v1/mechanic/refreshToken", refreshMechanicToken);
 
 

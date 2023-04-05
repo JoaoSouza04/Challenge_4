04b167dbf1e8a01fd17a8bb89e537c9a1e225446
João Pedro <joaosvieira04@gmail.com>
2023-04-05 10:58:07 -0300

    P2_Middlewares - Adicionando os middlewares de proteção das rotas, a partir dos tokens

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,56 @@
+import { Request, Response } from "express";
+import { string } from "joi";
+import jwt from 'jsonwebtoken';
+
+export const authClientToken = (req: Request, res: Response, next) => {
+  try {
+
+    const { authorization } = req.headers;
+    const token = authorization;
+    jwt.verify(token, process.env.CLIENT_SECRET);
+    next()
+
+  } catch (err) {
+    res.send(err);
+  }
+}
+
+export const authMechanicToken = (req: Request, res: Response, next) => {
+  try {
+    const { authorization } = req.headers;
+    const token = authorization;
+    jwt.verify(token, process.env.MECHANIC_SECRET);
+    next()
+
+  } catch (err) {
+    res.send(err);
+  }
+}
+
+export const authGeneralToken = (req: Request, res: Response, next) => {
+  try {
+    if (req.headers.client) {
+      const { client }: any = req.headers;
+      const token = client;
+      jwt.verify(token, process.env.CLIENT_SECRET);
+    }
+    else if (req.headers.mechanic) {
+      const { mechanic }: any = req.headers;
+      const token = mechanic;
+      jwt.verify(token, process.env.MECHANIC_SECRET);
+    }
+    else {
+      return res.status(400).json({
+        type: "Header Token",
+        Error: {
+          resource: "Name of the key, from the Token",
+          description: "Please name the key of the token as (client or Client) or (mechanic or Mechanic). Other names aren't accepted"
+        }
+      })
+    }
+    next();
+
+  } catch (err) {
+    res.send(err);
+  }
+}

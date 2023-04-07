c4d67a672320285cbce21eaccf5e25f5559f67f2
João Pedro <joaosvieira04@gmail.com>
2023-04-07 20:07:27 -0300

    P2_Controllers - Adicionando os status codes e arrumando/melhorando a lógica dos requests

--- src/controllers/partController.ts
+++ src/controllers/partController.ts
@@ -4,27 +4,45 @@ import { Part } from "../db/entities/part.entity";
 
 export const getAllParts = async (req: Request, res: Response) => {
   const parts = await myDataSource.getRepository(Part).find();
-  res.json(parts);
+  res.status(200).json(parts);
 }
 
 export const createPart = async (req: Request, res: Response) => {
   const part = await myDataSource.getRepository(Part).create(req.body);
   await myDataSource.getRepository(Part).save(part);
-  return res.json(part);
+  return res.status(201).json(part);
 }
 
 export const getOnePart = async (req: Request, res: Response) => {
-  const results = await myDataSource.getRepository(Part).findOneBy({
+  const part = await myDataSource.getRepository(Part).findOneBy({
     partId: req.params.partId,
   })
-  return res.send(results);
+
+  if (!part) return res.status(404).json({
+    type: "UUID",
+    Error: {
+      resource: "URL ID",
+      description: "Can't find the part with the part id entered!"
+    }
+  })
+
+  return res.status(200).send(part);
 }
 
 export const updatePart = async (req: Request, res: Response) => {
   const part = await myDataSource.getRepository(Part).findOneBy({
-    partId: req.params.partId,
+    partId: req.params.partId
   });
+
+  if (!part) return res.status(404).json({
+    type: "UUID",
+    Error: {
+      resource: "URL ID",
+      description: "Can't find the part with the part id entered!"
+    }
+  })
+
   await myDataSource.getRepository(Part).merge(part, req.body)
-  const results = await myDataSource.getRepository(Part).save(part);
-  return res.send(results);
+  const result = await myDataSource.getRepository(Part).save(part);
+  return res.status(200).send(result);
 }

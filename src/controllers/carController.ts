de507922f3c632dcfb0b23a060cd050adb6278af
João Pedro <joaosvieira04@gmail.com>
2023-04-07 20:04:43 -0300

    P2_Controllers - Adicionando os retornos de status codes para cada rota

--- src/controllers/carController.ts
+++ src/controllers/carController.ts
@@ -6,7 +6,7 @@ import { Client } from "../db/entities/client.entity";
 export const getAllCars = async (req: Request, res: Response) => {
   const results = await myDataSource.getRepository(Car).findBy(
     { ownerId: req.params.id });
-  return res.json(results);
+  return res.status(200).json(results);
 }
 
 export const createCar = async (req: Request, res: Response) => {
@@ -14,7 +14,7 @@ export const createCar = async (req: Request, res: Response) => {
     { where: { id: req.body.ownerId } });
 
   if (!client) {
-    return res.status(400).json({
+    return res.status(404).json({
       type: "Validation Error",
       error: {
         resource: "Client id",
@@ -35,7 +35,7 @@ export const createCar = async (req: Request, res: Response) => {
 
   const car = await myDataSource.getRepository(Car).create(req.body);
   await myDataSource.getRepository(Car).save(car);
-  return res.json(car);
+  return res.status(201).json(car);
 }
 
 export const updateCar = async (req: Request, res: Response) => {
@@ -43,11 +43,19 @@ export const updateCar = async (req: Request, res: Response) => {
     carId: req.params.carId
   });
 
+  if (!car) return res.status(404).json({
+    type: "UUID",
+    Error: {
+      resource: "URL ID",
+      description: "Can't find the car with the car id entered!"
+    }
+  });
+
   const client = await myDataSource.getRepository(Client).findOne(
     { where: { id: req.body.ownerId } });
 
   if (!client) {
-    return res.status(400).json({
+    return res.status(404).json({
       type: "Validation Error",
       error: {
         resource: "Client id",
@@ -68,54 +76,41 @@ export const updateCar = async (req: Request, res: Response) => {
 
   await myDataSource.getRepository(Car).merge(car, req.body)
   const results = await myDataSource.getRepository(Car).save(car);
-  return res.json(results);
+  return res.status(200).json(results);
 }
 
 export const deleteCar = async (req: Request, res: Response) => {
-  const client = await myDataSource.getRepository(Client).findOne(
-    { where: { id: req.body.ownerId } });
-
-  if (req.body.ownerId != req.params.id) {
-    return res.status(400).json({
-      type: "Validation Error",
-      error: {
-        resource: "Client id",
-        message: "The id of the URL and the ownerId aren't the same!"
-      }
-    })
-  }
-
-  if (!client) {
-    return res.status(400).json({
-      type: "Validation Error",
-      error: {
-        resource: "Client id",
-        message: "Invalid or not found id!"
-      }
-    });
-  }
-
   const car = await myDataSource.getRepository(Car).findBy({ carId: req.params.carId });
+
   const results = await myDataSource.getRepository(Car).delete(
     { carId: req.params.carId });
 
   if (results.affected == 0) return res.status(404).json({
     type: "Failure Search",
     error: {
-      resource: " Car id",
-      message: "The car was not found!"
+      resource: "Car id",
+      message: "Can't find the car with the id entered!"
     }
   });
 
-  return res.json({
+  return res.status(200).json({
     message: "This is what has been deleted!",
     Car: car
   })
 }
 
 export const getOneCar = async (req: Request, res: Response) => {
-  const results = await myDataSource.getRepository(Car).findBy(
+  const car = await myDataSource.getRepository(Car).findBy(
     { carId: req.params.carId }
   )
-  return res.json(results);
+
+  if (!car) return res.status(404).json({
+    type: "UUID",
+    Error: {
+      resource: "URL ID",
+      description: "Can't find the car with the car id entered!"
+    }
+  });
+
+  return res.status(200).json(car);
 }

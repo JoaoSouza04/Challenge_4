646ce5cc79b580867510e010448e7dba299e0264
João Pedro <joaosvieira04@gmail.com>
2023-04-07 20:06:20 -0300

    P2_Controllers - Adicionando os status codes e arrumando/melhorando a lógica dos requests

--- src/controllers/mechanicController.ts
+++ src/controllers/mechanicController.ts
@@ -9,31 +9,48 @@ export const getAllMechanics = async (req: Request, res: Response) => {
     mechanic.password = undefined;
   });
 
-  res.json(mechanics);
+  res.status(200).json(mechanics);
 }
 
 export const createMechanic = async (req: Request, res: Response) => {
   const mechanic = await myDataSource.getRepository(Mechanic).create(req.body);
   await myDataSource.getRepository(Mechanic).save(mechanic);
-  return res.json(mechanic);
+  return res.status(201).json(mechanic);
 }
 
 export const getOneMechanic = async (req: Request, res: Response) => {
-  const results = await myDataSource.getRepository(Mechanic).findOneBy({
+  const mechanic = await myDataSource.getRepository(Mechanic).findOneBy({
     id: req.params.id,
   })
 
-  results.password = undefined;
-  return res.json(results);
+  if (!mechanic) return res.status(404).json({
+    type: "UUID",
+    Error: {
+      resource: "URL ID",
+      description: "Can't find the mechanic with the mechanic id entered!"
+    }
+  })
+
+  mechanic.password = undefined;
+  return res.status(200).json(mechanic);
 }
 
 export const updateMechanic = async (req: Request, res: Response) => {
   const mechanic = await myDataSource.getRepository(Mechanic).findOneBy({
     id: req.params.id,
   });
+
+  if (!mechanic) return res.status(404).json({
+    type: "UUID",
+    error: {
+      resource: "URL ID",
+      message: "Can't find the mechanic with the mechanic id entered!"
+    }
+  });
+
   await myDataSource.getRepository(Mechanic).merge(mechanic, req.body)
   const results = await myDataSource.getRepository(Mechanic).save(mechanic);
 
   mechanic.password = undefined;
-  return res.json(results);
+  return res.status(200).json(results);
 }

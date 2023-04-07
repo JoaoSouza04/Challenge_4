fb5881dc1c068cd700c52248d80979e33b1f0aa7
João Pedro <joaosvieira04@gmail.com>
2023-04-07 20:10:31 -0300

    P2_Middlewares - Arrumando a lógica do arquivo e deixando o código mais conciso

--- src/middlewares/validateCarFields.ts
+++ src/middlewares/validateCarFields.ts
@@ -1,8 +1,7 @@
 import { Request, Response } from "express";
-import { validateCarData } from "../services/createCarValidation";
-import { validateCarUpdate } from "../services/updateCarValidation";
+import { validateCarData } from "../services/carValidation";
 
-export const validCreateCar = (req: Request, res: Response, next) => {
+export const validCar = (req: Request, res: Response, next) => {
   try {
     const valid = validateCarData(req.body);
     if (valid.error) return res.status(400).json({
@@ -18,20 +17,3 @@ export const validCreateCar = (req: Request, res: Response, next) => {
     return res.send(err)
   }
 }
-
-export const validUpdateCar = (req: Request, res: Response, next) => {
-  try {
-    const valid = validateCarUpdate(req.body);
-    if (valid.error) return res.status(400).json({
-      type: "Validation Error",
-      error: {
-        resource: "Request Body",
-        message: "The fields aren't valid"
-      }
-    });
-    next();
-
-  } catch (err) {
-    return res.send(err);
-  }
-} 

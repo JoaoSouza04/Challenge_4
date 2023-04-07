59c64ba3e7e50739c91d8ae8d1337cfc34e9e8e9
João Pedro <joaosvieira04@gmail.com>
2023-04-07 20:11:06 -0300

    P2_Middlewares - Arrumando a lógica do arquivo e deixando o código mais conciso

--- src/middlewares/validatePartsFields.ts
+++ src/middlewares/validatePartsFields.ts
@@ -1,27 +1,9 @@
 import { Request, Response } from "express";
-import { validatePartData } from "../services/createPartValidation";
-import { validatePartUpdate } from "../services/updatePartValidation";
+import { validatePartData } from "../services/partValidation";
 
-export const validCreatePart = (req: Request, res: Response, next) => {
+export const validPart = (req: Request, res: Response, next) => {
   try {
     const valid = validatePartData(req.body);
-    if (valid.error) return res.status(400).json({
-      type: "Validation Error",
-      error: {
-        resource: "Request Body",
-        message: "The fields aren't valid"
-      }
-    });
-    next();
-
-  } catch (err) {
-    return res.send(err);
-  }
-}
-
-export const validUpdatePart = (req: Request, res: Response, next) => {
-  try {
-    const valid = validatePartUpdate(req.body);
     if (valid.error) return res.status(400).json({
       type: "Validation Error",
       error: {

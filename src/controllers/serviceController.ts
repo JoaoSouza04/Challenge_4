7081811992279e683c04d18258507be411315414
João Pedro <joaosvieira04@gmail.com>
2023-04-07 20:07:41 -0300

    P2_Controllers - Adicionando os status codes e arrumando/melhorando a lógica dos requests

--- src/controllers/serviceController.ts
+++ src/controllers/serviceController.ts
@@ -8,7 +8,7 @@ import { Service } from "../db/entities/service.entity";
 
 export const getAllServices = async (req: Request, res: Response) => {
   const services = await myDataSource.getRepository(Service).find();
-  res.json(services);
+  res.status(200).json(services);
 }
 
 export const createService = async (req: Request, res: Response) => {
@@ -29,11 +29,11 @@ export const createService = async (req: Request, res: Response) => {
   const result = (!client || !mechanic || !car || !part) ? true : false;
 
   if (result) {
-    return res.status(400).json({
+    return res.status(404).json({
       type: "Validation Error",
       error: {
         resource: "UUID",
-        message: "Invalid or not found id!"
+        message: "Invalid or not found id's, please check the id's of Client, Mechanic, Car and Parts!"
       }
     });
   }
@@ -41,14 +41,23 @@ export const createService = async (req: Request, res: Response) => {
   const service = await myDataSource.getRepository(Service).create(req.body);
   await myDataSource.getRepository(Service).save(service);
 
-  return res.json(service);
+  return res.status(201).json(service);
 }
 
 export const getOneService = async (req: Request, res: Response) => {
-  const results = await myDataSource.getRepository(Service).findOneBy({
+  const service = await myDataSource.getRepository(Service).findOneBy({
     serviceId: req.params.serviceId,
-  })
-  return res.send(results);
+  });
+
+  if (!service) return res.status(404).json({
+    type: "UUID",
+    Error: {
+      resource: "URL ID",
+      description: "Can't find the service with the service id entered!"
+    }
+  });
+
+  return res.status(200).send(service);
 }
 
 export const updateService = async (req: Request, res: Response) => {
@@ -64,16 +73,16 @@ export const updateService = async (req: Request, res: Response) => {
 
   const parts = req.body.parts;
   const part = await myDataSource.getRepository(Part).findOne(
-    { where: { partId: parts.partId } })
+    { where: { partId: parts[0].partId } })
 
   const result = (!client || !mechanic || !car || !part) ? true : false;
 
   if (result) {
-    return res.status(400).json({
+    return res.status(404).json({
       type: "Validation Error",
       error: {
         resource: "UUID",
-        message: "Invalid or not found id!"
+        message: "Invalid or not found id's, please check the id's of Client, Mechanic, Car and Parts!"
       }
     });
   }
@@ -81,9 +90,18 @@ export const updateService = async (req: Request, res: Response) => {
   const service = await myDataSource.getRepository(Service).findOneBy({
     serviceId: req.params.serviceId,
   });
+
+  if (!service) return res.status(404).json({
+    type: "UUID",
+    Error: {
+      resource: "URL ID",
+      description: "Can't find the service with the service id entered!"
+    }
+  });
+
   await myDataSource.getRepository(Service).merge(service, req.body);
 
   service.totalPrice = parts[0].qtd * part.unitPrice
   const results = await myDataSource.getRepository(Service).save(service);
-  return res.json(results);
+  return res.status(200).json(results);
 }

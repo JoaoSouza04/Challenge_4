8cac849cdc5d4e2bee3246ea46ca4953aa9905e0
João Pedro <joaosvieira04@gmail.com>
2023-04-07 20:11:18 -0300

    P2_Middlewares - Arrumando a lógica do arquivo e deixando o código mais conciso

--- src/middlewares/validateServiceFields.ts
+++ src/middlewares/validateServiceFields.ts
@@ -1,27 +1,9 @@
 import { Request, Response } from "express";
-import { validateServiceData } from "../services/createServiceValidation";
-import { validateServiceUpdate } from "../services/updateServiceValidation"
+import { validateServiceData } from "../services/serviceValidation";
 
-export const validCreateService = (req: Request, res: Response, next) => {
+export const validService = (req: Request, res: Response, next) => {
   try {
     const valid = validateServiceData(req.body);
-    if (valid.error) return res.status(400).json({
-      type: "Validation Error",
-      error: {
-        resource: "Request Body",
-        message: "The fields aren't valid"
-      }
-    });
-    next();
-
-  } catch (err) {
-    return res.send(err);
-  }
-}
-
-export const validUpdateService = (req: Request, res: Response, next) => {
-  try {
-    const valid = validateServiceUpdate(req.body);
     if (valid.error) return res.status(400).json({
       type: "Validation Error",
       error: {
